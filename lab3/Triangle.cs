6a666a4c7e5b5506bcadaeca27435cfeae5c8f51
WrElisey <wrelisej@gmail.com>
2019-10-13 23:14:50 +0300

    add lab 3

---
+++ lab3/Triangle.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3
+{
+    public static class Triangle
+    {
+        public static bool IsExists(float a, float b, float c)
+        {
+            return (a + b > c) && (b + c > a) && (a + c > b);
+        }
+    }
+}
---
+++ lab3/TriangleTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Lab3
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void Test1_True()
+        {
+            bool expected = Triangle.IsExists(1, 1, 1);
+            Assert.IsTrue(expected);
+        }
+
+        [TestCase(200, 1, 300)]
+        public void Test2_False(float a, float b, float c)
+        {
+            bool expected = Triangle.IsExists(a, b, c);
+            Assert.IsTrue(!expected);
+        }
+
+        [TestCase(1.1f, 2.2f, 3.3f, ExpectedResult = true)]
+        [TestCase(287, 450, 200, ExpectedResult = true)]
+        [TestCase(368, 250, 200, ExpectedResult = true)]
+        [TestCase(546, 708, 200, ExpectedResult = true)]
+        [TestCase(0, 0, 0, ExpectedResult = false)]
+        [TestCase(5.1f, 1.2f, 3.3f, ExpectedResult = false)]
+        [TestCase(546, 708, 98, ExpectedResult = false)]
+        [TestCase(368, 250, 900, ExpectedResult = false)]
+        [TestCase(234, 2301, 200, ExpectedResult = false)]
+        [TestCase(776, 221, 200, ExpectedResult = false)]
+        [TestCase(133, 2275, 32, ExpectedResult = false)]
+        public bool Test3_MultiCase(float a, float b, float c)
+        {
+            bool expected = Triangle.IsExists(a, b, c);
+            return expected;
+        }
+    }
+}
